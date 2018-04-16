d9e4fad0b57161a94bd406c6493c754fd185d9b5
Glitch (twitchtv) <none>
2018-04-16 02:31:18 +0000

    👞🍭 Checkpoint ./public/client.js:789865/1304

--- public/client.js
+++ public/client.js
@@ -3,6 +3,11 @@
 
 function callServer(params, endpoint, callback) {
   var xhr = new XMLHttpRequest();
+  var url = new URL(endpoint);
+  for (var param in params) {
+    url.searchParams.append(param, params[param]);
+  }
+
   xhr.open( "GET", endpoint, false ); // false for synchronous request
   xhr.onreadystatechange = function() { 
     if (xhr.readyState == 4 && xhr.status == 200)
@@ -12,3 +17,22 @@ function callServer(params, endpoint, callback) {
   return xhr.responseText;
 }
 
+var Client = {
+  retrieve: function(command, params) {
+    callServer(params, Client[command].endpoint, Client[command].callback)
+  },
+  topGames: {
+    endpoint: "/topGames",
+    callback: function(data) {
+      console.log(data)
+    }
+  },
+  topStreams: {
+    endpoint: "/topStreams",
+    callback: function(data) {
+      console.log(data)
+    }
+  }
+}
+
+Client.retrieve({},"topGames")
