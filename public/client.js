6fec0e81c8ef861955fdee503b0f8511e957c593
Glitch (twitchtv) <none>
2018-04-16 06:05:09 +0000

    🐦🐊 Checkpoint ./public/client.js:789865/411

--- public/client.js
+++ public/client.js
@@ -3,19 +3,22 @@
 // run by the browser each time your view template is loaded
 
 function callServer(params, endpoint, callback) {
-  var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest(), response;
   endpoint += "?data=true"
   for (var param in params) {
     endpoint +=  "&" + param + "=" + params[param];
   }
   console.log("URL:" + endpoint)
   xhr.open( "GET", endpoint, true );
-  var response;
   xhr.onreadystatechange = function() { 
     if (xhr.readyState == 4 && xhr.status == 200)
-        respoxhr.responseText);
+        response = xhr.responseText;
   }
   xhr.send( null );
+  while(response === undefined) {
+    
+  }
+  
 }
 
 var Client = {
