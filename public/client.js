af72c7d9c1736c81c915faf1d57bbd84b56fffed
Glitch (twitchtv) <none>
2018-04-16 02:00:22 +0000

    🚲🏗 Checkpoint ./public/client.js:789865/787 ./views/index.html:789865/497

--- public/client.js
+++ public/client.js
@@ -1,11 +1,17 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-const Client = {
-  getTopGames: {
-    params: [],
-    endpoint: "/topGames",
-    callback: function(data) {}
+function callServer(params, endpoint, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open( "GET", endpoint, false ); // false for synchronous request
+  xhr.onreadystatechange = function() { 
+        if (xhr.readyState == 4 && xhr.status == 200)
+            callback(xhr.responseText);
   }
+  xhr.send( null );
+  return xhr.responseText;
 }
 
+const Client = {
+  
+}
