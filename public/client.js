0afd9bf0e2eaef4bcab0f5aa6560ab4f8667266d
Glitch (twitchtv) <none>
2018-04-16 02:21:10 +0000

    🌵😭 Checkpoint ./views/index.html:789865/735 ./server.js:789865/38 ./public/client.js:789865/87

--- public/client.js
+++ public/client.js
@@ -5,13 +5,10 @@ function callServer(params, endpoint, callback) {
   var xhr = new XMLHttpRequest();
   xhr.open( "GET", endpoint, false ); // false for synchronous request
   xhr.onreadystatechange = function() { 
-        if (xhr.readyState == 4 && xhr.status == 200)
-            callback(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == 200)
+        callback(xhr.responseText);
   }
   xhr.send( null );
   return xhr.responseText;
 }
 
-const Client = {
-  
-}
